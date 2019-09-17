ffd31913574d75d7740f88468358c01a1c4b1a4b
Just Me <lapaix360@hotmail.com>
2019-09-17 00:59:37 -0400

    Spring

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+//selecting the element to append the card function
+const cardContainer = document.querySelector(".cards-container");
+
+function createCard(newCard) {
+  const card = document.createElement("div");
+  const headLine = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const span = document.createElement("span");
+
+  //structuring
+  card.appendChild(headLine);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  author.appendChild(span);
+
+  //adding the classes to the elements
+  card.classList.add("card");
+  headLine.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  //adding the content
+
+  headLine.textContent = newCard;
+
+  return card;
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data.articles);
+  })
+
+  .catch(error => {
+    console.log("Error:", error);
+  });
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//topic element
+const topics = document.querySelector(".topics");
+
+//creating the component
+
+function createTab(tabItems) {
+  let tab = document.createElement("div");
+
+  tab.textContent = tabItems;
+  tab.classList.add("tab");
+
+  return tab;
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+
+    response.data.topics.forEach(item => {
+      let tabs = createTab(item);
+      topics.appendChild(tabs);
+    });
+  })
+
+  .catch(error => {
+    console.log("Error", error);
+  });
